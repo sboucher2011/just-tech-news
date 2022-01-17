9bcfc6f1cf3060187f6536dabf9f1b02df9c51ae
sboucher <sboucher2011@gmail.com>
2022-01-17 12:46:02 -0500

    add login updates up to 14.2.6

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,4 +38,23 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  router.get('/', (req, res) => {
+    console.log(req.session);
+  
+    // other logic...
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
 module.exports = router;
